187e0121343a6c508e76e2c0bdcd11c97bcf8d38
edomkinfe <edomkinfe19@gmail.com>
2022-03-11 22:27:29 -0600

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,18 +2,14 @@
 /**
  *main - entry point
  *Description: random number in n +ve/-ve
-#include <stdio.h>
-/**
- *main - entry point
- *Description: random number in n +ve/-ve
  *Return: 0 on success
  */
 int main(void)
 {
-int num, i;
-for (num = 48, i = 0; i < 10; num++, i++)
+int firstLetter, n;
+for (firstLetter = 122, n = 1; n < 27; firstLetter--, n++)
 {
-putchar(num);
+putchar(firstLetter);
 }
 putchar(10);
 return (0);
