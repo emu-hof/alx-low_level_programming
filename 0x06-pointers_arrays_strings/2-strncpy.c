e364d63930501f328dc94b26fefc5ee586618380
edomkinfe <edomkinfe19@gmail.com>
2022-03-24 23:36:03 -0600

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,21 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int byteCount;
+	int i;
 
-	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
-		dest[byteCount] = src[byteCount];
-		for (; byteCount < n; byteCount++)
-			dest[byteCount] = '\0';
+	i=0;
 
+	while(src[i] != '\0' && i<n)
+	{
+	  dest[i] = src[i];
+	  i++;
+	}
+
+	while(i<n)
+	{
+	  dest[i] = '\0';
+	  i++;
+	}
+	
 	return (dest);
 }
